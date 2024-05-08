5ad1d6de696b18b72e19129fa405c5ea617d29df
sreekutten <sreekuttenps5@gmail.com>
2024-05-08 21:37:22 +0530

    updated home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import landingImage from "../assets/landing.png";
 import appDownloadImage from "../assets/appDownload.png";
 import SearchBar, { SearchForm } from "@/components/SearchBar";
 import { useNavigate } from "react-router-dom";
-
 const HomePage = () => {
   const navigate = useNavigate();
 
